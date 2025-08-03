2077dc833069e57ec01f0f0407346ade7a841e82
beaxx <beatriz.oliveira260607@gmail.com>
2025-08-03 19:18:04 -0300

    banco de dados iniciado

---
+++ src/sql/dados.sql
@@ -0,0 +1,9 @@
+create database BeatrizLeticia;
+use BeatrizLeticia;
+
+create table Oliveira(
+    idprod primary key auto increment,
+    nome varchar not null,
+    imagem varchar not null,
+    preco decimal(5,2) not null
+);
