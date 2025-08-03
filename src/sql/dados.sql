f9030254c9c2031665357a81eb8056870a259906
beaxx <beatriz.oliveira260607@gmail.com>
2025-08-03 19:26:42 -0300

    fix: add table usuario

--- src/sql/dados.sql
+++ src/sql/dados.sql
@@ -8,3 +8,11 @@ create table Oliveira(
     preco decimal(5,2) not null
     descricao varchar not null
 );
+
+create table Usuario(
+    idusuario primary key auto increment,
+    nome varchar not null,
+    email varchar not null,
+    senha varchar not null,
+    type int not null -- 0 = admin, 1 = user
+);
