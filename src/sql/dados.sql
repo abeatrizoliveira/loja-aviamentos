dfb796fa658409c6c56c7a09f8d5feb3277dfb66
beaxx <beatriz.oliveira260607@gmail.com>
2025-08-03 19:45:51 -0300

    fix: update the tables

--- src/sql/dados.sql
+++ src/sql/dados.sql
@@ -1,18 +1,18 @@
 create database BeatrizLeticia;
 use BeatrizLeticia;
 
-create table Oliveira(
-    idprod primary key auto increment,
-    nome varchar not null,
-    imagem varchar not null,
-    preco decimal(5,2) not null
-    descricao varchar not null
+create table oliveira(
+	idprod int primary key auto_increment,
+    nome varchar(100) not null,
+	imagem varchar(255) not null,
+    preco decimal(5,2) not null,
+    descricao varchar(100) not null
 );
 
-create table Usuario(
-    idusuario primary key auto increment,
-    nome varchar not null,
-    email varchar not null,
-    senha varchar not null,
+create table usuario(
+	idusuario int primary key auto_increment,
+    nome varchar(100) not null,
+    email varchar(100) not null,
+    senha varchar(255) not null,
     type int not null -- 0 = admin, 1 = user
 );
