94a1c365597f21991129366db479f18c758ac97b
Beatriz <132356935+abeatrizoliveira@users.noreply.github.com>
2025-08-05 15:03:11 -0300

    Update dados.sql

--- src/sql/dados.sql
+++ src/sql/dados.sql
@@ -2,17 +2,16 @@ create database BeatrizLeticia;
 use BeatrizLeticia;
 
 create table oliveira(
-	idprod int primary key auto_increment,
+    idprod int primary key auto_increment,
     nome varchar(100) not null,
-	imagem varchar(255) not null,
+    imagem varchar(255) not null,
     preco decimal(5,2) not null,
     descricao varchar(100) not null
 );
 
 create table usuario(
-	idusuario int primary key auto_increment,
+    idusuario int primary key auto_increment,
     nome varchar(100) not null,
     email varchar(100) not null,
     senha varchar(255) not null,
-    type int not null -- 0 = admin, 1 = user
 );
