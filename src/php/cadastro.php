8755fe3c0ca86a6fb91aa67e030aa37aeb4c47c7
Beatriz <132356935+abeatrizoliveira@users.noreply.github.com>
2025-08-12 15:05:51 -0300

    Update cadastro.php

--- src/php/cadastro.php
+++ src/php/cadastro.php
@@ -4,10 +4,11 @@ include 'connection.php';
 session_start();
 
 $name = $_POST['adicionar aqui depois'];
+$email = $_POST['adicionar aqui depois'];
 $password = $_POST['adicionar aqui depois'];
 
-$sql = "INSERT INTO usuarios (name, password)
-VALUES ('$name', '$password')";
+$sql = "INSERT INTO usuarios (nome, email, senha)
+VALUES ('$name', '$email', '$password')";
 
 if(mysqli_query($mysqli, $sql)){
     header("Location: add depois");
